782b306d5e05e05ab14a97cedf8f7e333e582a0b
Администратор <danielstudentbsu@gmail.com>
2021-07-26 14:59:07 +0300

    Init first repository

---
+++ Repositories/GameRepository.cs
@@ -0,0 +1,20 @@
+﻿using GameAchieves.Models;
+using GameAchieves.Repositories.Contracts;
+using System;
+using System.Threading.Tasks;
+
+namespace GameAchieves.Repositories
+{
+    public class GameRepository : IGameRepository
+    {
+        public async Task<Game> GetAsync(Guid Id)
+        {
+            return new Game()
+            {
+                Name = "Minecraft",
+                Price = 0.55
+            };
+        }
+
+    }
+}
